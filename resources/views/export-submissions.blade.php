ccb2511fd1b144021bf8d108759632abbe1c3e19
Tom Broucke <tom@tombroucke.be>
2023-11-02 14:18:14 +0100

    added translations

--- resources/views/export-submissions.blade.php
+++ resources/views/export-submissions.blade.php
@@ -1,4 +1,4 @@
-<h2><?php echo __( 'Export submissions', 'html-forms' ); ?></h2>
+<h2><?php echo __( 'Export submissions', 'html-forms-export-submissions' ); ?></h2>
 
 @foreach($services as $serviceKey => $service)
   <a href="{{ $service->exportLink() }}" class="button">{!! sprintf(__('Export to %s file', 'html-forms-export-submissions' ), $service->label()) !!}</a>
---
+++ sage-html-forms-export-submissions.php
@@ -0,0 +1,6 @@
+<?php
+/*
+ * Plugin Name: Sage HTML Forms Export Submissions
+ * Text Domain: html-forms-export-submissions
+ * Domain Path: /resources/lang
+ */
